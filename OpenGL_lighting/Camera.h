7ea1ab57b9a07e0d408ba011c94fdc1ef4f40a83
Guilherme Schüler <guilherme.gschuler@gmail.com>
2022-08-25 19:47:42 -0300

    Camera class added.

---
+++ OpenGL_lighting/Camera.h
@@ -0,0 +1,45 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
+class Camera 
+{
+    void updateCameraVectors();
+public:
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+    float Yaw;
+    float Pitch;
+    float MovSpeed;
+    float MouseSensitivity;
+    float Zoom;
+
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM);
+    Camera(float xPos, float yPos, float zPos, float xUp, float yUp, float zUp, float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM);
+    glm::mat4 getViewMatrix();
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessMouseMov(float xOffset, float yOffset, GLboolean constrainPitch = true);
+    void ProcessMouseScroll(float yOffset);
+};
+
+#endif 
